491aa1dd5bf16244ed3b9c7a5101796c1fa8aef4
Karina Garces ✨ <karinadgarces@gmail.com>
2023-08-14 18:05:46 -0400

    grab hero from webapi

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -41,14 +41,20 @@ private handleError<T>(operation = 'operation', result?: T) {
     // update to retrieve data from http 
     return this.http.get<Hero[]>(this.heroesUrl)
     .pipe(
+      tap(_ => this.log('fetched heroes')),
       catchError(this.handleError<Hero[]>('getHeroes', []))
     );
 }
 
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    )
+    // const hero = HEROES.find(h => h.id === id)!;
+    // this.messageService.add(`HeroService: fetched hero id=${id}`);
+    // return of(hero);
   }
 
   /** Log a HeroService message with the MessageService */
