917ffb81e0754bc7d333551b5b4e973d5e866b59
Karina Garces ✨ <karinadgarces@gmail.com>
2023-08-15 19:59:40 -0400

    add hero functional

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -67,7 +67,14 @@ private handleError<T>(operation = 'operation', result?: T) {
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
-  
+
+  /** POST: add a new hero to the server */
+addHero(hero: Hero): Observable<Hero> {
+  return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+    tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+    catchError(this.handleError<Hero>('addHero'))
+  );
+}
   /** Log a HeroService message with the MessageService */
 private log(message: string) {
   this.messageService.add(`HeroService: ${message}`);
